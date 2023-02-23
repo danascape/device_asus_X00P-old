a35c9f08a6400d2cf370003df148aeb4601e8ab7
me-cafebabe <me.cafebabe@gmail.com>
2023-02-24 04:14:43 +0800

    X00P: Legacy devices doesn't support ICE Encryption
    Change-Id: I07003e50aea9a8fe5bf59437e769166c8dcd4668
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -158,6 +158,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
+# Encryption
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.crypto.volume.contents_mode=aes-256-xts
+
 # For config.fs
 PRODUCT_PACKAGES += \
     fs_config_files
